89184f068d159263d5f61de71f43b0ac235338a8
bella.golod <bella.golod@danziger.co.il>
2026-07-02 15:18:31 +0300

    Initial commit - FullStack project setup

---
+++ backend/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowFrontend");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+
+
+function App() {
+
+
+
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
